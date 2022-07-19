bf532fb948a9691c62e1e81fb9c6126da9034543
eljezer <jezerrazuri@upeu.edu.pe>
2022-07-19 17:54:17 -0500

    hola

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
+app.use((req, res, next) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  console.log("IP:", ip);
+  next();
+});
 
 app.get("/", function (req, res) {
   res.send("Bienvenido a la API de SISLIC");
