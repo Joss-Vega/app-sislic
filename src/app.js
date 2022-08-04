03dda1aa16e612162f1e51a8d5164d721a92c512
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2022-08-04 09:49:32 -0500

    AF

--- src/app.js
+++ src/app.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 var cors = require("cors");
-const path = require("path")
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(morgan("combined"));
-console.log();
-app.use("/uploads", express.static(path.join(__dirname, "..","uploads")));
-
-app.use((req, res, next) => {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  console.log("IP:", ip);
-  next();
-});
 
 app.get("/", function (req, res) {
   res.send("Bienvenido a la API de SISLIC");
